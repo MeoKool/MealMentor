1e9a1326ec57a009cefffa4aec69671dc0b1edf5
MeoKool <truongsyquang1202@gmail.com>
2024-10-04 22:51:06 +0700

    favourite screen

--- lib/screens/Setting/editProfile.dart
+++ lib/screens/Setting/editProfile.dart
@@ -1,30 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:mealmentor/screens/Setting/profile_screen.dart';
+import 'package:mealmentor/screens/navigation_menu.dart';
 
 class EditProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFB5D6A0), // light green
+        title: Text("Thông tin cá nhân", style: TextStyle(color: Colors.grey)),
+        backgroundColor: Color(0xFF9dc08b),
         elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            // Handle back action
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ProfileScreen()),
-            );
-          },
-        ),
-        title: Text(
-          'Thông tin cá nhân',
-          style: TextStyle(color: Colors.black),
-        ),
+        centerTitle: false,
       ),
       body: Container(
-        color: Color(0xFFB5D6A0), // same light green background
+        color: Color(0xFF9dc08b), // same light green background
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
